50fa839b8c235f24fd2a540c6f2ac874948eeaef
Jacob Sørensen <JacobSchwartzSoerensen@gmail.com>
2014-05-22 01:21:31 +0200

    Restructuring folders

---
+++ net/minelegion/gunmod/CommonProxy.java
@@ -0,0 +1,12 @@
+package net.minelegion.gunmod;
+
+public class CommonProxy {
+
+	//Client stuff
+	public void registerRenderers(){
+		
+		//Nothing here as the server doesn't render graphics or entities!
+		
+	}
+	
+}
